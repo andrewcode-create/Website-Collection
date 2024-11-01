11f02c35c892049d5c892ac99fb09d285c67dece
Andrew Haas <andrew.haas2020@gmail.com>
2024-11-01 08:23:34 -0500

    changed to jsx from typescript

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+//import { useState } from "react";
+//import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <iframe
+          src="https://example.com/"
+          width={window.innerWidth}
+          height={window.innerHeight}
+        ></iframe>
+      </div>
+    </>
+  );
+}
+
+export default App;
